5ef9cd0cf6b8fbdc9c0fa022c848fd793b2b2315
Michael Ostermann <michaelostermann@me.com>
2026-05-27 13:50:59 +0200

    build: fix errors

--- packages/unplugin-string/tsdown.config.ts
+++ packages/unplugin-string/tsdown.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "tsdown"
 
 export default defineConfig({
     clean: true,
+    deps: { neverBundle: ["vite"] },
     dts: true,
     entry: ["src/*.ts"],
     format: ["esm", "cjs"],
